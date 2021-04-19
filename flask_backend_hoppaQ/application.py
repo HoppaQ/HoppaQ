43a81413b2f4f78bc1d27b8c825fdc37e09b431e
Utsav Singla <utsav18321@iiitd.ac.in>
2021-04-19 14:16:47 +0530

    changes

---
+++ flask_backend_hoppaQ/application.py
@@ -0,0 +1,64 @@
+# Start with a basic flask app webpage.
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+from random import random
+from time import sleep
+from threading import Thread, Event
+
+__author__ = 'Utsav'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+#turn the flask app into a socketio app
+socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+
+#random number Generator Thread
+thread = Thread()
+thread_stop_event = Event()
+
+def randomNumberGenerator():
+    """
+    Generate a random number every 1 second and emit to a socketio instance (broadcast)
+    Ideally to be run in a separate thread?
+    """
+    #infinite loop of magical random numbers
+    print("Making random numbers")
+    while not thread_stop_event.isSet():
+        with open("./newfile.csv", "r") as f:
+            text = f.read()
+            f.close()
+        print(text)
+        listX = text.split(",")
+        name = listX[0]
+        brandName = listX[1]
+        qty = listX[2]
+        price = listX[3]
+        socketio.emit('newnumber', {'name': name, 'brandName':brandName ,'qty':qty, 'price':price}, namespace='/test')
+        socketio.sleep(5)
+
+
+@app.route('/')
+def index():
+    #only by sending this page first will the client be connected to the socketio instance
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    # need visibility of the global thread object
+    global thread
+    print('Client connected')
+
+    #Start the random number generator thread only if the thread has not been started before.
+    if not thread.isAlive():
+        print("Starting Thread")
+        thread = socketio.start_background_task(randomNumberGenerator)
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
